af5f5d5861745a140bdcca2a3213d1fa8a69d8d6
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-06-03 03:41:41 +0200

    created Address model"

---
+++ db/migrate/20210603014043_create_addresses.rb
@@ -0,0 +1,16 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :full_name
+      t.string :mobile_number
+      t.string :country
+      t.string :state
+      t.string :city_code
+      t.string :city_name
+      t.address :street
+
+      t.timestamps
+    end
+  end
+end
