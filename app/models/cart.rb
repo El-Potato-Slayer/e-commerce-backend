d7e98eff419da4f46fff920ec0645c8056a67eab
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-06-03 03:02:51 +0200

    added method to create Order and clear Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :products, class_name: 'CartProduct'
+
+  def create_order() 
+    order = Order.create(user_id: user.id, has_shipped: false)
+
+    products.map do |cart_product|
+      OrderProduct.create(order_id: order.id, product_id: cart_product.product.id, quantity: cart_product.quantity)
+    end
+
+    CartProduct.where(cart_id: id).destroy_all
+  end
 end
